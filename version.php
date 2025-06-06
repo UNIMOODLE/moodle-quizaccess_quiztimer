61afb5b1c91df2c51608271e72d8f252858a8dc1
Jminero <135690501+jmineroisyc@users.noreply.github.com>
2025-06-06 08:45:13 +0200

    Update version.php

--- version.php
+++ version.php
@@ -34,7 +34,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_quiztimer';
-$plugin->release = '1.0.1';
-$plugin->version = 2024032001;
+$plugin->release = '1.0.2';
+$plugin->version = 2025060601;
 $plugin->requires = 2022041900;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
